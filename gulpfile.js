424d58b2731dca69167a35477a62e9f70dab96e8
Illia Maksymov <ivashthestampede01@gmail.com>
2017-12-08 16:43:40 +0200

    Добавил плагин gulp-pug

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,21 @@
 'use strict';
 
 const gulp = require('gulp');
+const pug = require('gulp-pug');
+
+const paths = {
+    root: './build',
+    templates: {
+        pages: 'src/templates/pages/*.pug',
+        src: 'src/templates/**/*.pug'
+    }
+};
+
+// pug
+function templates() {
+    return gulp.src(paths.templates.pages)
+        .pipe(pug({ pretty: true }))
+        .pipe(gulp.dest(paths.root));
+};
+
+exports.templates = templates;
