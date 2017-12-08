0d103217c5233f00d61e6c6eb70f867971edcf9b
Illia Maksymov <ivashthestampede01@gmail.com>
2017-12-08 18:06:58 +0200

    Добавил таск styles

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,20 @@
 const gulp = require('gulp');
 const pug = require('gulp-pug');
 
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+
 const paths = {
     root: './build',
     templates: {
         pages: 'src/templates/pages/*.pug',
         src: 'src/templates/**/*.pug'
+    },
+    styles: {
+        src: 'src/styles/**/*.scss',
+        dest: 'build/assets/styles/'
     }
 };
 
@@ -18,4 +27,19 @@ function templates() {
         .pipe(gulp.dest(paths.root));
 };
 
+// scss
+function styles() {
+    return gulp.src('./src/styles/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(paths.styles.dest));
+};
+
 exports.templates = templates;
+exports.styles = styles;
