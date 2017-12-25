5f4cae9ba3037be55864564608727cb045392998
Illia Maksymov <ivashthestampede01@gmail.com>
2017-12-25 11:38:15 +0200

    Сделал черновой вариант flip анимации

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -20,3 +20,24 @@ const moveLayers = e => {
 }
 
 window.addEventListener('mousemove', moveLayers);
+
+
+// flip animation
+
+const trigger = document.getElementById('authorisation-btn');
+const flipper = document.querySelector('.welcome__flipper');
+const backBtn = document.querySelector('.back');
+const rotate = 180;
+
+const flip = e => {
+    e.preventDefault();
+    flipper.style.transform = `rotateY(${rotate}deg)`;
+}
+
+const back = e => {
+    e.preventDefault();
+    flipper.style.transform = `rotateY(0)`;
+}
+
+trigger.addEventListener('click', flip);
+backBtn.addEventListener('click', back);
